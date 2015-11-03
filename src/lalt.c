d9ac0fee0b58ab3d9f790ee322f43003652d5b6d
Ian Remsen <ian@ianremsen.com>
2015-11-03 03:13:52 -0500

    created separate method

--- src/lalt.c
+++ src/lalt.c
@@ -7,9 +7,46 @@
 
 char* concat(char* s1, char* s2) {
     char* result = malloc(strlen(s1) + strlen(s2) + 1);
+
     strcpy(result, s1);
     strcat(result, s2);
     return result;
+}
+
+void copyFile(char* filename) {
+    FILE* ifp;
+    FILE* ofp;
+    char outputFilename[255];
+    char inputFilename[255];
+    char buffer;
+
+    strcpy(inputFilename, filename);
+
+    ifp = fopen(inputFilename, "r");
+    if (ifp == NULL) {
+        fprintf(stderr, "Can't open input file %s.\n", inputFilename);
+        exit(1);
+    }
+
+    strcpy(outputFilename, concat((char*) inputFilename, ".ahk"));
+    ofp = fopen(outputFilename, "w+");
+    if (ofp == NULL) {
+        fprintf(stderr, "Can't open output file %s.\n", outputFilename);
+        exit(1);
+    }
+
+    do {
+        buffer = fgetc(ifp);
+        if (buffer != 0xffffffff) {
+            fputc(buffer, ofp);
+        }
+
+    } while (buffer != EOF);
+
+    fclose(ifp);
+    fclose(ofp);
+    printf("Input file %s copied to output file %s successfully.\n", inputFilename, outputFilename);
+    exit(0);
 }
 
 int main(int argc, char** argv) {
@@ -17,7 +54,6 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Error, no parameter specified.\n");
         exit(1);
     }
-
     if (strcmp(argv[1], "repl") == 0) {
         lalt_repl();
     } else if (strcmp(argv[1], "compile") == 0) {
@@ -27,40 +63,7 @@ int main(int argc, char** argv) {
             exit(1);
         }
 
-        FILE* ifp;
-        FILE* ofp;
-        char outputFilename[255];
-        char inputFilename[255];
-        char buffer;
-
-        strcpy(inputFilename, argv[2]);
-
-        ifp = fopen(inputFilename, "r");
-
-        if (ifp == NULL) {
-            fprintf(stderr, "Can't open input file %s.\n", inputFilename);
-            exit(1);
-        }
-
-        strcpy(outputFilename, concat((char*) inputFilename, ".ahk"));
-        ofp = fopen(outputFilename, "w+");
-        if (ofp == NULL) {
-            fprintf(stderr, "Can't open output file %s.\n", outputFilename);
-            exit(1);
-        }
-
-        do {
-            buffer = fgetc(ifp);
-            if (buffer != 0xffffffff) {
-                fputc(buffer, ofp);
-            }
-
-        } while (buffer != EOF);
-
-        fclose(ifp);
-        fclose(ofp);
-        printf("Input file %s copied to output file %s successfully.\n", inputFilename, outputFilename);
-        exit(0);
+        copyFile(argv[2]);
     } else {
         fprintf(stderr, "Error, invalid parameter %s.\n", argv[1]);
         exit(1);
